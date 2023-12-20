20432de42fd12ad12a35f9eea1ea8ff1544d80f0
njdhdl1223 <a08tw04b05@gmail.com>
2023-12-20 22:54:24 +0800

    Refector test

--- src/components/NameSetting/NameSetting.jsx
+++ src/components/NameSetting/NameSetting.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import '../../styles/name-setting.scss'
 import BirthSetting from './BirthSetting'
 import GenderSetting from './GenderSetting'
 
+const baseUrl = 'http://localhost:8080';
+
 export default function NameSetting() {
+
+// const response = axios.get(`${baseUrl}/chatgpt/getNames`);
+axios.get(`http://localhost:8080/chatgpt/getNames`)
+  .then(function (response){
+    console.log(response.data)
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
+
+axios.get(`https://randomuser.me/api/?results=3&gender=female`)
+  .then(function (response){
+    console.log(response.data.results[0])
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
+
+
+  // const [generatedName, setGenerateName] = useState('wait...');
+
+
+
+  // useEffect(() => {
+  //   const fetchNames = async () => {
+  //     try {
+  //       const response = await axios.get(`${baseUrl}/chatgpt/getNames`);
+        
+  //       console.log(JSON.stringify(response.data));
+  //       const name = response.data
+  //       setGenerateName(name)
+  //       return response
+  //     }
+  //     catch(error){
+  //       console.error('API call failed', error)
+  //     }
+  //   }
+
+  //   fetchNames();
+  // }, [])
+
+  // const [selectedGender, setSelectedGender] = useState('')
+  // const [selectedDate, setSelectedDate] = useState('')
+  
+  // const handleGenderChange = (gender) => {
+  //   setSelectedGender(gender)
+  // }
+  // const handleDateChange = (date) => {
+  //   setSelectedDate(date)
+  // }
+
+  // const sendDataToBackend = async () => {
+  //   try {
+  //     const response = await axios.post(`${baseUrl}/sendData`, {
+  //       data : selectedDate,
+  //     })
+  //     console.log(response.data)
+  //   } catch (error) {
+  //     console.error('API call failed', error)
+  //   }
+  // }
+
+
   return (
     <div className="container">
       <Header title="Name setting"/>
