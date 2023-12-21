18d423c02a7ecba360d96a4a726ffb7ab6f72724
njdhdl1223 <a08tw04b05@gmail.com>
2023-12-21 10:57:40 +0800

    Fix axios

--- src/components/NameSetting/NameSetting.jsx
+++ src/components/NameSetting/NameSetting.jsx
@@ -6,75 +6,17 @@ import '../../styles/name-setting.scss'
 import BirthSetting from './BirthSetting'
 import GenderSetting from './GenderSetting'
 
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'http://localhost:8888';
 
 export default function NameSetting() {
 
-// 修改api header
-axios.defaults.headers = {
-  'Content-Type': 'application/json; charset=utf-8'
-};
-
-axios.get(`http://localhost:8080`)
-  .then(function (response){
-    console.log(response)
-  })
-  .catch(function (error) {
-    console.log(error)
-  })
-
-// axios.get(`https://randomuser.me/api/?results=3&gender=female`)
-//   .then(function (response){
-//     console.log(response.data)
-//   })
-//   .catch(function (error) {
-//     console.log(error)
-//   })
-
-
-  // const [generatedName, setGenerateName] = useState('wait...');
-
-
-
-  // useEffect(() => {
-  //   const fetchNames = async () => {
-  //     try {
-  //       const response = await axios.get(`${baseUrl}/chatgpt/getNames`);
-        
-  //       console.log(JSON.stringify(response.data));
-  //       const name = response.data
-  //       setGenerateName(name)
-  //       return response
-  //     }
-  //     catch(error){
-  //       console.error('API call failed', error)
-  //     }
-  //   }
-
-  //   fetchNames();
-  // }, [])
-
-  // const [selectedGender, setSelectedGender] = useState('')
-  // const [selectedDate, setSelectedDate] = useState('')
-  
-  // const handleGenderChange = (gender) => {
-  //   setSelectedGender(gender)
-  // }
-  // const handleDateChange = (date) => {
-  //   setSelectedDate(date)
-  // }
-
-  // const sendDataToBackend = async () => {
-  //   try {
-  //     const response = await axios.post(`${baseUrl}/sendData`, {
-  //       data : selectedDate,
-  //     })
-  //     console.log(response.data)
-  //   } catch (error) {
-  //     console.error('API call failed', error)
-  //   }
-  // }
-
+axios.get(`${baseUrl}/gemini/getNames`)
+  .then( response =>
+    console.log(response.data.Data.name)
+  )
+  .catch( err =>
+    console.log(err)
+  )
 
   return (
     <div className="container">
@@ -89,7 +31,7 @@ axios.get(`http://localhost:8080`)
           <section className="generate">
             <div className="generate-container d-flex flex-column justify-content-center align-items-center">
               <div className="generate-text">Your name is:</div>
-              <div className="generate-result">Wait for generate</div>
+              <div className="generate-result"></div>
               <button className="generate-btn cursor-pointer">
                 <div>Generate</div>
                 <img className="gpt-img" src="/image/chatgpt.png" width="40px" alt="ChatGPT.logo" />
