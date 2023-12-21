0f8f0ee7d87d2c52efc4f239d3764cbe3d47f609
njdhdl1223 <a08tw04b05@gmail.com>
2023-12-22 00:05:07 +0800

    feat generate name by gender

--- src/components/NameSetting/GenderSetting.jsx
+++ src/components/NameSetting/GenderSetting.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function GenderSetting() {
+export default function GenderSetting({onGenderSelected}) {
+  const [selectedGender,setSelectedGender] = useState('')
+
+  const handleGenderChange = (event) => {
+    const genderValue = event.target.value
+    setSelectedGender(genderValue)
+    onGenderSelected(genderValue)
+  }
   return (
     <section className="gender">
       <div className="gender-container">
         <div className="gender-text text-center">Gender</div>
         <div className="radio-container d-flex justify-content-between">
           <div className="radio-male-container d-flex flex-column justify-content-center align-items-center">
-            <input type="radio" name="gender" defaultValue="male"/>
+            <input type="radio" name="gender" className="cursor-pointer" value="male" onChange={handleGenderChange}/>
             <div className="checkbox-male-text">Male</div>
           </div>
           <div className="radio-female-container d-flex flex-column justify-content-center align-items-center">
-            <input type="radio" name="gender" defaultValue="female"/>
+            <input type="radio" name="gender" className="cursor-pointer" value="female" onChange={handleGenderChange}/>
             <div className="checkbox-male-text">Female</div>
           </div>
         </div>
--- src/components/NameSetting/NameSetting.jsx
+++ src/components/NameSetting/NameSetting.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
@@ -7,16 +7,30 @@ import BirthSetting from './BirthSetting'
 import GenderSetting from './GenderSetting'
 
 const baseUrl = 'http://localhost:8888';
+const birthdate = '19961223'
 
 export default function NameSetting() {
+  const [generatedName, setGeneratedName] = useState('')
+  const [selectedGender, setSelectedGender] = useState('')
 
-axios.get(`${baseUrl}/gemini/getNames`)
-  .then( response =>
-    console.log(response.data.Data.name)
-  )
-  .catch( err =>
-    console.log(err)
-  )
+  const handleGenderSelected = (gender) => {
+    setSelectedGender(gender)
+  }
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(`${baseUrl}/gemini/getNames?birthdate=${birthdate}&gender=${selectedGender}`)
+      let result = res.data.Data.name
+      console.log(res.data)
+      setGeneratedName(result)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  fetchData()
+}, [selectedGender])
 
   return (
     <div className="container">
@@ -26,12 +40,12 @@ axios.get(`${baseUrl}/gemini/getNames`)
         <div className="name-setting"></div>
         <div className="name-setting-container h100 d-flex flex-column justify-content-evenly align-items-center">
           <div className="title">Tell me something about you !</div>
-          <GenderSetting/>
+          <GenderSetting onGenderSelected={handleGenderSelected}/>
           <BirthSetting/>
           <section className="generate">
             <div className="generate-container d-flex flex-column justify-content-center align-items-center">
               <div className="generate-text">Your name is:</div>
-              <div className="generate-result"></div>
+              <div className="generate-result">{generatedName}</div>
               <button className="generate-btn cursor-pointer">
                 <div>Generate</div>
                 <img className="gpt-img" src="/image/chatgpt.png" width="40px" alt="ChatGPT.logo" />
