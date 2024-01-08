50c45b2bf9871c0fd2b1284ec4501bc291df0ca9
njdhdl1223 <a08tw04b05@gmail.com>
2024-01-08 13:13:25 +0800

    Fix delete the import of unfinished component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NameSetting from "./components/NameSetting/NameSetting";
-import SelectRoom from "./components/SelectRoom/SelectRoom";
 
 export default function App() {
   return (
