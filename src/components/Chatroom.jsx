0877ef7e0c0001035f343a873d546ccfa6d7b71a
njdhdl1223 <a08tw04b05@gmail.com>
2024-01-29 21:18:41 +0800

    Refactor: establish WebSocket connection with URL-based Authentication

--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import useWebSocket, { ReadyState } from "react-use-websocket";
 import Cookies from "js-cookie";
 import Header from "./Header/Header";
 import "../styles/chatroom.scss";
@@ -8,9 +7,6 @@ export default function Chatroom() {
   const [inputText, setInputText] = useState("");
   const [chatHistory, setChatHistory] = useState([]);
   const [generatedToken, setGeneratedToken] = useState("");
-  const { sendMessage, lastMessage, readyState } = useWebSocket(
-    "ws://localhost:8080/ws"
-  );
 
   useEffect(() => {
     const token = Cookies.get("token");
@@ -18,17 +14,37 @@ export default function Chatroom() {
     setGeneratedToken(token);
   }, []);
 
-  console.log(generatedToken);
+  useEffect(() => {
+    // 建立WebSocket連接
+    const ws = new WebSocket(
+      `ws://localhost:8080/ws?Sec-WebSocket-Token=${generatedToken}`
+    );
 
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: "Connecting",
-    [ReadyState.OPEN]: "Open",
-    [ReadyState.CLOSING]: "Closing",
-    [ReadyState.CLOSED]: "Closed",
-    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
-  }[readyState];
+    // 事件處理器 - 連接建立
+    ws.onopen = (event) => {
+      console.log("WebSocket connected.");
+    };
 
-  console.log(connectionStatus);
+    // 事件處理器 - 接收訊息
+    ws.onmessage = (event) => {
+      console.log("Received message:", event.data);
+    };
+
+    // 事件處理器 - 連接關閉
+    ws.onclose = (event) => {
+      console.log("WebSocket closed.");
+    };
+
+    // 事件處理器 - 連接錯誤
+    ws.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    // 在 component 卸載時關閉 WebSocket 連接
+    return () => {
+      ws.close();
+    };
+  }, [generatedToken]);
 
   const handleInputText = (event) => {
     setInputText(event.target.value);
