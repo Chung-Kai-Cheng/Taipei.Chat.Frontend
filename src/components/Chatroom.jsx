72643cc6355dc6b5f8b23d203f754e562af4f7cf
njdhdl1223 <a08tw04b05@gmail.com>
2024-02-07 15:17:58 +0800

    feat: add send & received messages function in Chatroom

--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import Header from "./Header/Header";
@@ -7,11 +7,17 @@ import SendRoundedIcon from "@mui/icons-material/SendRounded";
 
 export default function Chatroom() {
   const navigate = useNavigate();
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [webSocket, setWebSocket] = useState(null);
+
   useEffect(() => {
     const token = Cookies.get("chat-token");
 
     // 建立WebSocket連接
     const ws = new WebSocket(`ws://localhost:8080/ws?chat-token=${token}`);
+
+    setWebSocket(ws);
 
     //每1分鐘定期檢查Cookies中的token是否存在
     const intervalId = setInterval(() => {
@@ -28,7 +34,10 @@ export default function Chatroom() {
 
     // 事件處理器 - 接收訊息
     ws.onmessage = (event) => {
-      console.log("Received message:", event.data);
+      //標註為接收的訊息
+      const receivedMessage = { content: event.data, type: "received" };
+      // 更新聊天訊息
+      setMessages((prevMessages) => [...prevMessages, receivedMessage]);
     };
 
     // 事件處理器 - 連接關閉
@@ -53,17 +62,59 @@ export default function Chatroom() {
     };
   }, []);
 
+  const handleEnterPress = (event) => {
+    if (event.key === "Enter") {
+      sendMessage();
+    }
+  };
+
+  const sendMessage = () => {
+    if (inputValue.trim() !== "" && webSocket.readyState === WebSocket.OPEN) {
+      webSocket.send(inputValue);
+
+      //標註為傳送的訊息
+      const sentMessage = { content: inputValue, type: "sent" };
+      // 更新聊天消息
+      setMessages((prevMessages) => [...prevMessages, sentMessage]);
+      // 清空输入框
+      setInputValue("");
+    }
+  };
+
   return (
     <div className="container">
       <Header title="Chatroom" />
       <main>
-        <div className="chat-space d-flex flex-column align-items-end"></div>
+        <div className="chat-space d-flex flex-column">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`chat-box ${message.type}`}
+              //傳送的訊息顯示於右邊，接收的訊息顯示於左邊
+              style={{
+                alignSelf: `flex-${message.type === "sent" ? "end" : "start"}`,
+              }}
+            >
+              {message.content}
+            </div>
+          ))}
+        </div>
       </main>
       <footer className="type-place">
         <div className="input-container">
-          <input type="text" placeholder="" className="chat-input" />
+          <input
+            type="text"
+            placeholder=""
+            className="chat-input"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={handleEnterPress}
+          />
           <span className="icon-container">
-            <button className="send-message-btn cursor-pointer">
+            <button
+              className="send-message-btn cursor-pointer"
+              onClick={sendMessage}
+            >
               <SendRoundedIcon
                 sx={{
                   fontSize: 22,
